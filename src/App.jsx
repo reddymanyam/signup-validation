5db9ff28b8f511b2b3b6b0f965258b417541f25d
reddymanyam <reddappareddy18@gmail.com>
2025-01-24 17:30:07 +0530

    app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Auth from './Auth/Auth'
 import ResponsiveSidebar from './Sidebar/Sidebar'
+import Login from './Sidebar/Login'
 
 const App = () => {
   return (
     <>
      {/* <Auth /> */}
-     <ResponsiveSidebar />
+     {/* <ResponsiveSidebar /> */}
+     <Login />
     </>
   )
 }
