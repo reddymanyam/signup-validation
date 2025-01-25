1ddef553c11333ece9bc83d71f2f9f097a0e999b
reddymanyam <reddappareddy18@gmail.com>
2025-01-26 01:24:21 +0530

    App

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,16 @@
 import React from 'react'
 import Auth from './Auth/Auth'
 import ResponsiveSidebar from './Sidebar/Sidebar'
-<<<<<<< HEAD
 import GenerateCircle from './GenerateCircle/GenerateCircle'
-=======
 import Login from './Sidebar/Login'
->>>>>>> eaa59902517d33c632a57bb5b3645a17c004d425
 
 const App = () => {
   return (
     <>
      {/* <Auth /> */}
      {/* <ResponsiveSidebar /> */}
-<<<<<<< HEAD
      <GenerateCircle />
-=======
      <Login />
->>>>>>> eaa59902517d33c632a57bb5b3645a17c004d425
     </>
   )
 }
